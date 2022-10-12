de70709932d801c89dc926ec87929e2302d382a9
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-12 21:53:58 +0200

    ej 1 codigo encontrado

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -1,2 +1,16 @@
+import sympy
+
 def main1():
-    print()
+    # Resolviendo ecuación diferencial
+    # Defino las incognitas
+    x = sympy.Symbol('x')
+    y = sympy.Function('y')
+
+    # Expreso la ecuacion
+    f = 6*x**2 - 3*x**2*(y(x))
+    sympy.Eq(y(x).diff(x), f)
+
+
+
+if __name__=='__main__':
+    main1()
