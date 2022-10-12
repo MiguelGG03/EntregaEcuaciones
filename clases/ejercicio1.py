dfcc3306b7c3ac707b5194c6f9984dd683698543
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-12 22:37:27 +0200

    guardar intentos en el ejercicio 1

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -2,15 +2,16 @@ import sympy
 
 def main1():
     # Resolviendo ecuación diferencial
-    # Defino las incognitas
+    # defino las incognitas
     x = sympy.Symbol('x')
     y = sympy.Function('y')
 
-    # Expreso la ecuacion
+    # expreso la ecuacion
     f = 6*x**2 - 3*x**2*(y(x))
     sympy.Eq(y(x).diff(x), f)
 
-
+    # Resolviendo la ecuación
+    sympy.dsolve(y(x).diff(x) - f)
 
 if __name__=='__main__':
     main1()
