47e6f801f371b697c2080d2e834e6d2e258ea08f
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-13 21:47:31 +0200

    empiezo el 4 a ver que tal sale

--- clases/ejercicio4.py
+++ clases/ejercicio4.py
@@ -1,2 +1,14 @@
+# IMPORTAR TODAS LAS FUNCIONES DE LA LIBRERÍA SYMPY
+from sympy import *
+# Visualización de resultados en forma de ecuación:
+# Utilizará Látex para la escritura de fórmulas, si no se tiene látex, por defecto se usa MathJax
+from sympy.interactive import printing
+
+printing.init_printing(use_latex=True)
+
 def main4():
-    print()
+
+
+
+if __name__=='__main__':
+    main4()
