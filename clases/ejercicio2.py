292659d18b270611dc1caf6bea244697e1adaa8f
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-13 22:31:38 +0200

    no me van los imports, que triste

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -1,2 +1,31 @@
+# IMPORTAR TODAS LAS FUNCIONES DE LA LIBRERÍA SYMPY
+from sympy import *
+# Visualización de resultados en forma de ecuación:
+# Utilizará Látex para la escritura de fórmulas, si no se tiene látex, por defecto se usa MathJax
+from sympy.interactive import printing
+
+printing.init_printing(use_latex=True)
+
+# Imprimir línea vacía para espaciar fórmulas
+
+def lin():
+  print('\n') # imprime línea vacía
+
 def main2():
-    print()
+
+    t=symbols('t')
+    y=Function('y')('t')
+
+    ecuacion=diff(y,t)-(y/(t-2))-2*(t-2)**2
+    solucion=dsolve(ecuacion)
+    
+
+    print('============ ENUNCIADO =============')
+    lin()
+    pprint(ecuacion)
+    lin()
+    print('============ SOLUCION =============')
+    lin()
+    pprint(solucion)
+    lin()
+    print('=================================')
