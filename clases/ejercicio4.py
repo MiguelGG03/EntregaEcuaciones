090bdda955108f6007acde70e5d9ca90dd1b0a54
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-13 22:14:55 +0200

    ejercicio 4 completado

--- clases/ejercicio4.py
+++ clases/ejercicio4.py
@@ -6,8 +6,28 @@ from sympy.interactive import printing
 
 printing.init_printing(use_latex=True)
 
-def main4():
+# Imprimir línea vacía para espaciar fórmulas
+def lin():
+  print('\n') # imprime línea vacía
 
+def main4():
+    t=symbols('t')
+    y=Function('y')('t')
+
+    ecuacion=2*t*diff(y,t)-y-3*(t)**2
+    solucion=dsolve(ecuacion)
+    
+
+    print('============ ENUNCIADO =============')
+    lin()
+    pprint(ecuacion)
+    lin()
+    print('============ SOLUCION =============')
+    lin()
+    pprint(solucion)
+    lin()
+    print('=================================')
+    
 
 
 if __name__=='__main__':
